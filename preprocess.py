8f0d5f7ee7ce8093f8faf61d3aff2d634af70a48
Michael Du <mdu7@stanford.edu>
2020-03-20 22:19:14 -0500

    change preprocess

--- preprocess.py
+++ preprocess.py
@@ -9,8 +9,8 @@ files_list = [f for f in listdir("labels") if isfile(join("labels", f))]
 #     with open('gun.txt', 'a') as fout:
 #         fout.write("images/" + filename[0:-3] + "JPEG\n")
 for filepath in files_list:
-    # with open(filepath, 'r') as fin:
-    #     data = fin.read().splitlines(True)
+    with open(filepath, 'r') as fin:
+        data = fin.read().splitlines(True)
     #     print(data)
     with open(filepath, 'w+') as fout:
         for line in data[1:]:
